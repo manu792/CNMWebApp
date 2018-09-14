628ff876c9c366fef50025548f6937c3e8935207
Manuel Roman <MRoman@tradestation.com>
2018-09-14 13:25:50 -0600

    initial push

---
+++ CNMWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CNMWebApp.Startup))]
+namespace CNMWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
