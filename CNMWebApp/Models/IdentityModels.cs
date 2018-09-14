e3ff39827d8a305907e019d2bb4d8ebd32c97837
Manuel Roman <MRoman@tradestation.com>
2018-09-14 15:53:07 -0600

    add database initializer to set default manager user and role

--- CNMWebApp/Models/IdentityModels.cs
+++ CNMWebApp/Models/IdentityModels.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -18,11 +20,55 @@ namespace CNMWebApp.Models
         }
     }
 
+    public class Initializer : CreateDatabaseIfNotExists<ApplicationDbContext>
+    {
+        public Initializer()
+        {
+            
+        }
+        protected override void Seed(ApplicationDbContext context)
+        {
+            context.Roles.Add(new IdentityRole()
+            {
+                Id = "1",
+                Name = "Manager"
+            });
+            context.Roles.Add(new IdentityRole()
+            {
+                Id = "2",
+                Name = "Empleado"
+            });
+            context.SaveChanges();
+
+            CreateUserAndAssignRole(context);
+        }
+
+        private void CreateUserAndAssignRole(DbContext context)
+        {
+            try
+            {
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+                var user = new ApplicationUser() { UserName = "manager@manager.com" };
+
+                var task = userManager.CreateAsync(user, "Manager123.");
+                var result = task.Result;
+                var currentUser = userManager.FindByName(user.UserName);
+                var roleresult = userManager.AddToRole(currentUser.Id, "Manager");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+            Database.SetInitializer(new Initializer());
         }
 
         public static ApplicationDbContext Create()
