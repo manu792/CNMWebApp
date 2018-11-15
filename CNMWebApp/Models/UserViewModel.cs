01c439e476eca49f2fb535a2fa7587b3a91f74c9
Manuel Roman <MRoman@tradestation.com>
2018-11-15 15:06:04 -0600

    refactor code

--- CNMWebApp/Models/UserViewModel.cs
+++ CNMWebApp/Models/UserViewModel.cs
@@ -44,8 +44,8 @@ namespace CNMWebApp.Models
         [DisplayName("Superusuario")]
         public bool EsSuperusuario { get; set; }
         [DisplayName("Saldo Días Disponibles")]
-        [RegularExpression(@"^[0-9]\d*(\.\d+)?$", ErrorMessage = "Debe ser un valor numérico")]
-        [Range(0, 1000, ErrorMessage = "Debe ser un número entre 0 y 1000")]
+        [RegularExpression(@"^-?[0-9]\d*(\.\d+)?$", ErrorMessage = "Debe ser un valor numérico")]
+        // [Range(0, 1000, ErrorMessage = "Debe ser un número entre 0 y 1000")]
         [Required(ErrorMessage = "El campo Saldo Días Disponibles es requerido")]
         [DefaultValue(0)]
         public decimal SaldoDiasDisponibles { get; set; }
